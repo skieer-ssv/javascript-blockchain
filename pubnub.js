c83a50ef036fc6bbe38d04825856608d09dd1c40
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2021-09-10 02:10:56 +0530

    checking git alias for commit all

--- pubnub.js
+++ pubnub.js
@@ -4,7 +4,7 @@ const credentials = {
   publishKey: "pub-c-14544d74-310f-42c8-b748-33065fab7e2a",
   subscribeKey: "sub-c-bb14d736-11a5-11ec-83e9-0e85f81976b6",
   secretKey: "sec-c-NmQzODk1NTktZTIxMC00ZjVlLWJhYjYtYzgwNzI0OGQwOTVk",
-};
+}; 
 const CHANNELS = {
   TEST: "TEST",
 };
