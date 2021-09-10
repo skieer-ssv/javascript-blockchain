cec91564fbed6ae5cf9e3001178c3215c194e825
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2021-09-10 20:16:24 +0530

    synchronized peers and removed redundancy

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Blockchain = require('./blockchain');
-const PubSub = require('./pubsub-redis');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Blockchain = require("./blockchain");
+const PubSub = require("./pubsub-redis");
+const request = require("request");
 
-const app= express();
-const blockchain= new Blockchain();
+const app = express();
+const blockchain = new Blockchain();
 const pubsub = new PubSub(blockchain);
 
-setTimeout(()=> pubsub.broadcastChain(),3000); 
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+
 
 app.use(bodyParser.json());
 
-app.get('/api/blocks',(req,res)=>{
-    res.json(blockchain.chain);
-
+app.get("/api/blocks", (req, res) => {
+  res.json(blockchain.chain);
 });
 
-app.post('/api/mine',(req,res)=>{
-const {data}=req.body;
-blockchain.addBlock({data});
-res.redirect('/api/blocks');
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  pubsub.broadcastChain();
+  res.redirect("/api/blocks");
 });
 
+const syncChains = () => {
+  request(
+    {
+      url: `${ROOT_NODE_ADDRESS}/api/blocks`,
+    },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        console.log("replace chain on a sync with ", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
 
-const PORT=3000;
-app.listen(PORT,()=>{
-    console.log(`Listening at localhost:${PORT}`)
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+app.listen(PORT, () => {
+  console.log(`Listening at localhost:${PORT}`);
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
 });
-
--- pubsub-redis.js
+++ pubsub-redis.js
@@ -1,47 +1,52 @@
-    const redis = require('redis');
-    const Blockchain = require('./blockchain');
-    const CHANNELS = {
-        TEST:'TEST',
-        BLOCKCHAIN:'BLOCKCHAIN'
+const redis = require("redis");
+const Blockchain = require("./blockchain");
+const CHANNELS = {
+  TEST: "TEST",
+  BLOCKCHAIN: "BLOCKCHAIN",
+};
+
+class PubSub {
+  constructor(blockchain) {
+    this.blockchain = blockchain;
+
+    this.publisher = redis.createClient();
+    this.subscriber = redis.createClient();
+
+    this.subscribeToChannels();
+    this.subscriber.on("message", (channel, message) =>
+      this.handleMessage(channel, message)
+    );
+  }
+
+  handleMessage(channel, message) {
+    console.log(`Message Received. channel: ${channel}. Message: ${message}`);
+    const parsedMessage = JSON.parse(message);
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      this.blockchain.replaceChain(parsedMessage);
     }
+  }
 
-    class PubSub{
-        constructor(blockchain){
-            this.blockchain = blockchain;
-         
-            this.publisher = redis.createClient();
-            this.subscriber = redis.createClient();
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach((channel) => {
+      this.subscriber.subscribe(channel);
+    });
+  }
 
-            this.subscribeToChannels();
-            this.subscriber.on('message', (channel,message)=>this.handleMessage(channel,message));
-        }
+  publish({ channel, message }) {
+      this.subscriber.unsubscribe(channel,()=>{
+        this.publisher.publish(channel, message,()=>{
+            this.subscriber.subscribe(channel);
+        });
+      });
+    
+  }
 
-        handleMessage(channel,message){
-            console.log(`Message Received. channel: ${channel}. Message: ${message}`);
-            const parsedMessage = JSON.parse(message);
-            if (channel=== CHANNELS.BLOCKCHAIN){
-                this.blockchain.replaceChain(parsedMessage);
-              }
-        }
-
-        subscribeToChannels() {
-            Object.values(CHANNELS).forEach(channel => {
-              this.subscriber.subscribe(channel);
-            });
-          }
-        publish({channel, message}){
-            this.publisher.publish(channel, message);
-        }
-
-        broadcastChain(){
-            
-            this.publish({
-                channel:CHANNELS.BLOCKCHAIN,
-                message: JSON.stringify(this.blockchain.chain)
-            });
-        }
-    }
-
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
+  }
+}
 
 module.exports = PubSub;
-
