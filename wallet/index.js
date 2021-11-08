343dd6aec4b159ea867b3563a730ea6d01827b1b
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2021-11-08 19:25:53 +0530

    added "update function" to the transaction class

--- wallet/index.js
+++ wallet/index.js
@@ -1,12 +1,13 @@
 const {STARTING_BALANCE} = require('../config');
 const {ec, cryptoHash} = require('../util');
+const Transaction = require('./transaction');
 
 class Wallet {
     constructor(){
         this.balance = STARTING_BALANCE;
 
         this.keyPair =  ec.genKeyPair();
-
+ 
         
         // this.publicKey = this.keyPair.getPublic("hex");
         this.publicKey= this.keyPair.getPublic().encode("hex"); 
@@ -15,6 +16,13 @@ class Wallet {
 sign(data){
 return this.keyPair.sign(cryptoHash(data));
     }
+    
+createTransaction({recipient,amount}){
+    if(amount>this.balance){
+        throw new Error('Amount exceeds balance');
+    }
+    return new Transaction({senderWallet:this,recipient,amount});
+}
 }
 
 module.exports = Wallet;
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require("./index");
 const { verifySignature } = require("../util");
+const Transaction = require('./transaction');
 describe("Wallet", () => {
   let wallet;
   beforeEach(() => {
@@ -33,6 +34,30 @@ describe("Wallet", () => {
           signature: new Wallet().sign(data),
         })
       ).toBe(false);
+    });
+  });
+  describe('createTransaction()',()=>{
+    describe('and the amount exceeds the balance',()=>{
+      it('throws an error',()=>{
+expect(()=> wallet.createTransaction({amount: 99999999, recipient: 'sid-recipient'})).toThrow('Amount exceeds balance');
+      });
+    });
+    describe('and the amount is valid',()=>{
+      let transaction,amount,recipient;
+      beforeEach(()=>{
+        amount = 50;
+        recipient='sid-recipient';
+        transaction= wallet.createTransaction({amount, recipient});
+      })
+      it('creates an instance of `Transaction`',()=>{
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+      it('matches the transaction input with the wallet',()=>{
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+      it('outputs the amount to the recipient',()=>{
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
   });
 });
